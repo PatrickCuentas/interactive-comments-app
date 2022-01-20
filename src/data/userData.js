f0986a0ddfe01d4d64a4db3ceff8489ef391f3b4
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-01-19 19:45:36 -0500

    Update userData.js

--- src/data/userData.js
+++ src/data/userData.js
@@ -13,7 +13,7 @@ export const userData = {
       content:
         "Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
       createdAt: "1 month ago",
-      score: 24,
+      score: 12,
       user: {
         image: {
           png: "https://i.imgur.com/UFx8rrs.png",
@@ -27,7 +27,7 @@ export const userData = {
       content:
         "Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
       createdAt: "2 weeks ago",
-      score: 25,
+      score: 5,
       user: {
         image: {
           png: "https://i.imgur.com/ncRJuXn.png",
