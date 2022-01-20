be2a2722bad63df1c81285ada520475b38033f2d
patrick_cuentas <patrickcuentasmariano@gmail.com>
2022-01-19 19:44:06 -0500

    Update Form.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -7,19 +7,23 @@ import { addComment } from "../actions/addComment.jsx";
 export const Form = () => {
     const {comments,dispatch,currentUser} = useContext(UserContext);
     const [inputValue,handleInput,handleSubmit] = useForm("");
-    const {user:{image:{png}}} = currentUser;
+    const {user:{image:{png},username}} = currentUser;
 
 
     return (
-    <form onSubmit={(e)=>handleSubmit(e,addComment,{
-        inputValue,
-        currentUser,
-        comments,
-        dispatch
-    })}>
-       <div className="grid form gap-5 my-5 p-5 bg-white rounded-2xl lg:place-content-between">
-        <textarea
-          className="
+      <form
+        onSubmit={(e) =>
+          handleSubmit(e, addComment, {
+            inputValue,
+            currentUser,
+            comments,
+            dispatch,
+          })
+        }
+      >
+        <div className="grid form gap-5 my-5 p-5 bg-white rounded-2xl lg:place-content-between">
+          <textarea
+            className="
             form-control
             w-full
             px-3
@@ -36,27 +40,26 @@ export const Form = () => {
             focus:light-gray
             focus:text-gray-700  focus:outline-none
           "
-          id="exampleFormControlTextarea1"
-          rows="3"
-          placeholder="Add a comment..."
-          value={inputValue}
-          onChange={handleInput}
-        >
-        </textarea>
-            <div className="profile">
-                <a href="#">
-                      <img
-                        className="h-10"
-                        src={`../.${png}`} alt="" />
-                </a>
-            </div>
-            <div className="send place-self-end lg:place-self-start">
-                <button
-                    className="px-8 py-4 lg:py-2.5 rounded-xl bg-moderate-blue text-white text-xl lg:text-lg font-bold hover:opacity-40"
-                    type="submit"
-                >SEND</button>
-            </div>
-    </div>
-    </form>
-    )
+            id="exampleFormControlTextarea1"
+            rows="3"
+            placeholder="Add a comment..."
+            value={inputValue}
+            onChange={handleInput}
+          ></textarea>
+          <div className="profile">
+            <a href="#">
+              <img className="h-10" src={png} alt={`${username} photo`} />
+            </a>
+          </div>
+          <div className="send place-self-end lg:place-self-start">
+            <button
+              className="px-8 py-4 lg:py-2.5 rounded-xl bg-moderate-blue text-white text-xl lg:text-lg font-bold hover:opacity-40"
+              type="submit"
+            >
+              SEND
+            </button>
+          </div>
+        </div>
+      </form>
+    );
 }
